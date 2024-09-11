13ed14ff6619381889ccb2f77375c3de052b4916
Alexey Postnov <adpostnov@yandex.ru>
2024-09-11 12:02:42 +0500

    Постнов Алексей. Задание 06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,23 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int result = x + y;
+        int cnt = 0;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+         while (result != 0) {
+             cnt++;
+             result /= 10;
+         }
 
-        return 0;
+        if (cnt == 0) return 1;
+
+
+        return cnt;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
