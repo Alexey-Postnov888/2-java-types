37b065ec8bf066e5d745fb232a30a266eba4accf
Alexey Postnov <adpostnov@yandex.ru>
2024-09-11 11:02:21 +0500

    Постнов Алексей. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,23 @@
 package com.example.task02;
 
+import java.util.Scanner;
+
 public class Task02 {
-
     public static String solution(String input) {
+        long value = Long.parseLong(input);
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        if (value >= Byte.MIN_VALUE && value <= Byte.MAX_VALUE) return "byte";
+        else if (value >= Short.MIN_VALUE && value <= Short.MAX_VALUE) return "short";
+        else if (value >= Integer.MIN_VALUE && value <= Integer.MAX_VALUE) return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        String result = solution(scanner.nextLine());
+        System.out.println("Type of your number: " + result);
     }
 
 }
